44731d3dfa8c9cbf394c56c9cbc619c6497bdf31
Mlikar <115452874+Mlikar@users.noreply.github.com>
2024-01-10 12:08:46 +0000

    Oprava

--- Výuka_C++/Prohození/prohozeni.cpp
+++ Výuka_C++/Prohození/prohozeni.cpp
@@ -4,13 +4,26 @@ using namespace std;
 
 int main()
 {
-    int a = 2;
+/*    int a = 2;
     int b = 1;
     int value;
 
     value = a;
     a = b;
     b = value;
+
+    cout << a << " " << b;
+*/
+
+    int a = 2;
+    int b = 1;
+
+    int* _a = &a;
+    int* _b = &b;
+
+    int value = *_a;
+    *_a = *_b;
+    *_b = value;
 
     cout << a << " " << b;
 
