7ec00f7822b8b70249407b9daed6d8514e9b548c
Mlikar <115452874+Mlikar@users.noreply.github.com>
2024-01-19 09:43:20 +0000

    oprava2_0

--- Výuka_C++/Prohození/prohozeni.cpp
+++ Výuka_C++/Prohození/prohozeni.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+void prohozeni(int* a, int* b)
+{
+    int promena = *a;
+    *a = *b;
+    *b = promena;
+}
+
 int main()
 {
 /*    int a = 2;
@@ -13,7 +20,7 @@ int main()
     b = value;
 
     cout << a << " " << b;
-*/
+
 
     int a = 1;
     int b = 0;
@@ -26,5 +33,11 @@ int main()
     *_b = value;
 
     cout << a << " " << b;
+*/
+int a, b;
 
+int* c = &a;
+int* d = &b;
+
+prohozeni(c, d);
 }
